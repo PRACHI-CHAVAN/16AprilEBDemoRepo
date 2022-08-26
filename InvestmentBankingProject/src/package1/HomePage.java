41b5129e90653efa2ad4f57ffca028cbb883aa54
PRACHI CHAVAN <chavan.prachi7187@gmail.com>
2022-08-27 00:26:58 +0530

    Feature new
    copmleted feature branch by prachi

--- InvestmentBankingProject/src/package1/HomePage.java
+++ InvestmentBankingProject/src/package1/HomePage.java
@@ -3,7 +3,7 @@ package package1;
 public class HomePage {
 
 	public static void main(String[] args) {
-		System.out.println("Homepage");
+		System.out.println("homepage");
 
 	}
 
---
+++ InvestmentBankingProject/src/packagechange/Newcode.java
@@ -0,0 +1,10 @@
+package packagechange;
+
+public class Newcode {
+
+	public static void main(String[] args) {
+		System.out.println("prachi");
+
+	}
+
+}
